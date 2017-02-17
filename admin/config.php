f3d38cd079e8da13018d02ba1220f84cf86e1c87
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-02-17 09:26:25 +0300

    add clean opencart to project

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/buildapp/admin/');
+define('HTTP_CATALOG', 'http://localhost/buildapp/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/buildapp/admin/');
+define('HTTPS_CATALOG', 'http://localhost/buildapp/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/WebServers/www/buildapp/admin/');
+define('DIR_SYSTEM', 'C:/WebServers/www/buildapp/system/');
+define('DIR_LANGUAGE', 'C:/WebServers/www/buildapp/admin/language/');
+define('DIR_TEMPLATE', 'C:/WebServers/www/buildapp/admin/view/template/');
+define('DIR_CONFIG', 'C:/WebServers/www/buildapp/system/config/');
+define('DIR_IMAGE', 'C:/WebServers/www/buildapp/image/');
+define('DIR_CACHE', 'C:/WebServers/www/buildapp/system/cache/');
+define('DIR_DOWNLOAD', 'C:/WebServers/www/buildapp/system/download/');
+define('DIR_UPLOAD', 'C:/WebServers/www/buildapp/system/upload/');
+define('DIR_LOGS', 'C:/WebServers/www/buildapp/system/logs/');
+define('DIR_MODIFICATION', 'C:/WebServers/www/buildapp/system/modification/');
+define('DIR_CATALOG', 'C:/WebServers/www/buildapp/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dragon_gate');
+define('DB_PASSWORD', 'dragongatepass');
+define('DB_DATABASE', 'build_app');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/buildapp/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/buildapp/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/WebServers/www/buildapp/catalog/');
+define('DIR_SYSTEM', 'C:/WebServers/www/buildapp/system/');
+define('DIR_LANGUAGE', 'C:/WebServers/www/buildapp/catalog/language/');
+define('DIR_TEMPLATE', 'C:/WebServers/www/buildapp/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/WebServers/www/buildapp/system/config/');
+define('DIR_IMAGE', 'C:/WebServers/www/buildapp/image/');
+define('DIR_CACHE', 'C:/WebServers/www/buildapp/system/cache/');
+define('DIR_DOWNLOAD', 'C:/WebServers/www/buildapp/system/download/');
+define('DIR_UPLOAD', 'C:/WebServers/www/buildapp/system/upload/');
+define('DIR_MODIFICATION', 'C:/WebServers/www/buildapp/system/modification/');
+define('DIR_LOGS', 'C:/WebServers/www/buildapp/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dragon_gate');
+define('DB_PASSWORD', 'dragongatepass');
+define('DB_DATABASE', 'build_app');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
