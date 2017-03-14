6490155efa69777bafb9e5d1060642183f10f7a5
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-03-14 13:39:57 +0200

    fixes

--- catalog/controller/account/edit.php
+++ catalog/controller/account/edit.php
@@ -201,7 +201,7 @@ class ControllerAccountEdit extends Controller
         }
 
         if (($this->customer->getEmail() != $this->request->post['email']) && $this->model_account_customer->getTotalCustomersByEmail($this->request->post['email'])) {
-            $this->error['warning'] = 'error_exists';
+            $this->error['warning'] = 'error_email_exists';
         }
 
         if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
